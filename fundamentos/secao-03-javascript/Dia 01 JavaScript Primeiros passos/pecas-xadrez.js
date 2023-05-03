797f8ddc0de8bd57aa332caf2d3a635c4d7ba161
felipesegala <felipe_segala@outlook.com>
2023-05-03 14:25:39 -0300

    add default e toLowercase

--- fundamentos/secao-03-javascript/Dia 01 JavaScript Primeiros passos/pecas-xadrez.js
+++ fundamentos/secao-03-javascript/Dia 01 JavaScript Primeiros passos/pecas-xadrez.js
@@ -1,16 +1,24 @@
-let chessPiece = 'cavalo';
+let chessPiece = 'PEao';
 
-switch(chessPiece){
-    case 'peao': console.log('uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas');
-    break
-    case 'cavalo': console.log('duas casas na horizontal e uma na vertical, ou duas na vertical e uma na horizontal, uma na horizontal e duas na vertical, e assim por diante');
-    break
-    case 'torre': console.log("quantas casas quiser na vertical e horizontal");
-    break
-    case 'rainha': console.log("Rainha -> Quantas casas quiser em qualquer direção");
-    break
-    case 'rei': console.log("Rei -> Uma casa em qualquer direção");
-    break
-    case 'bispo': console.log('Bispo -> Diagonal');
-    break
+switch (chessPiece.toLowerCase()) {
+    case 'peao':
+        console.log('Peão -> Uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas');
+        break
+    case 'cavalo':
+        console.log('Cavalo -> Duas casas na horizontal e uma na vertical, ou duas na vertical e uma na horizontal, uma na horizontal e duas na vertical, e assim por diante');
+        break
+    case 'torre':
+        console.log("Torre -> Quantas casas quiser na vertical e horizontal");
+        break
+    case 'rainha':
+        console.log("Rainha -> Quantas casas quiser em qualquer direção");
+        break
+    case 'rei':
+        console.log("Rei -> Uma casa em qualquer direção");
+        break
+    case 'bispo':
+        console.log('Bispo -> Diagonal');
+        break
+    default:
+        console.log('Erro, peça inesistente');
 }
