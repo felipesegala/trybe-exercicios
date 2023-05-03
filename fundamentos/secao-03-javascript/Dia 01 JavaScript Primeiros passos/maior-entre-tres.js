b00fbd00e65722544133a107506c0a7fc015789c
felipesegala <felipe_segala@outlook.com>
2023-05-03 07:57:54 -0300

    maior de tres

---
+++ fundamentos/secao-03-javascript/Dia 01 JavaScript Primeiros passos/maior-entre-tres.js
@@ -0,0 +1,11 @@
+let a = 2;
+let b = 3;
+let c = 4;
+
+if(a > b && a > c){
+    console.log("A é maior");
+} else if (b > a && b > c){
+    console.log("B é maior");
+}else {
+    console.log("C é maior");
+}
