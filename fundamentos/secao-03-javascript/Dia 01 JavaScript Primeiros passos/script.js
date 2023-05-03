b90fb1166d1c50e91d8038fe5f1bdb46126c7e99
felipesegala <felipe_segala@outlook.com>
2023-05-03 07:49:20 -0300

    ex op aritimeticos

---
+++ fundamentos/secao-03-javascript/Dia 01 JavaScript Primeiros passos/script.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 5;
+
+let soma = a + b;
+let soma1 = a - b;
+let soma2 = a * b;
+let soma3 = a / b;
+let soma4 = a % b;
+console.log(soma);
+console.log(soma1);
+console.log(soma2);
+console.log(soma3);
+console.log(soma4);
