da34e4b1de7dc7532f2a34dd626d24770dc3c942
felipesegala <felipe_segala@outlook.com>
2023-05-03 15:49:36 -0300

    mostrada divisão por dois do indice

--- fundamentos/secao-03-javascript/Dia 02 JavaScript Array e loop For/de1a25.js
+++ fundamentos/secao-03-javascript/Dia 02 JavaScript Array e loop For/de1a25.js
@@ -3,5 +3,9 @@ let numbers = [];
 for(let index = 0; index <= 25; index += 1){
     numbers.push(index);
 }
-
 console.log(numbers);
+
+for(let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index] / 2);
+}
+
