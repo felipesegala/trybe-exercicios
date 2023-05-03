c7cdeb0daf79c974b2c7cd7ae76d3d76218764ec
felipesegala <felipe_segala@outlook.com>
2023-05-03 10:14:53 -0300

    for

--- fundamentos/secao-03-javascript/Dia 02 JavaScript Array e loop For/arrays.js
+++ fundamentos/secao-03-javascript/Dia 02 JavaScript Array e loop For/arrays.js
@@ -12,6 +12,24 @@
 // console.log(menuServices);
 //mostrar um item especifico
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato')
-console.log(menu);
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
+// console.log(menu);
+
+// let numero = 7;
+
+// for(contador = 1; contador <= 10; contador += 1){
+//     console.log(numero * contador);
+// }
+// exemplo de tabuada
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for(index = 0; index < groceryList.length; index += 1){
+//     console.log(groceryList[index]);
+// }
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(let name of names){
+    console.log(name);
+}
