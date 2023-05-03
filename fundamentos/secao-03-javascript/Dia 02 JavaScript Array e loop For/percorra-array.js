49c6cc63fb1286fc2e445ac4cfe14bbfa302f518
felipesegala <felipe_segala@outlook.com>
2023-05-03 15:17:31 -0300

    verifica se maior q 20

--- fundamentos/secao-03-javascript/Dia 02 JavaScript Array e loop For/percorra-array.js
+++ fundamentos/secao-03-javascript/Dia 02 JavaScript Array e loop For/percorra-array.js
@@ -5,3 +5,9 @@ for(let index = 0; index < numbers.length; index += 1){
      sum += numbers[index];
 }
     console.log(sum / 10);
+
+    if(sum > 20){
+        console.log('O valor da média aritmética é maior que 20');
+    } else {
+        console.log('O valor da média aritmética é menor ou igual a 20');
+    }
