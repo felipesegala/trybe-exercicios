98ab30b11b20eed71db7539278422d8085d3211a
felipesegala <felipe_segala@outlook.com>
2023-05-03 09:40:41 -0300

    exemplos de arrays

---
+++ fundamentos/secao-03-javascript/Dia 02 JavaScript Array e loop For/arrays.js
@@ -0,0 +1,17 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// tasksList.push('Almoço');
+
+// for(let index = 0; index < tasksList.length; index += 1){
+//     console.log(tasksList[index]);
+// }
+
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[2];
+
+// console.log(menuServices);
+//mostrar um item especifico
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+console.log(menu);
