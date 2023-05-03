87339ed97e9db63c97ca52234233c9a218c6665f
felipesegala <felipe_segala@outlook.com>
2023-05-03 15:59:30 -0300

    feito mais sem entender direito conceito

---
+++ fundamentos/secao-03-javascript/Dia 02 JavaScript Array e loop For/fatorial.js
@@ -0,0 +1,9 @@
+//O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero.
+
+let number = 1;
+
+for (let index = 10; index > 0; index -= 1){
+    number *= 1;
+}
+
+console.log(number);
