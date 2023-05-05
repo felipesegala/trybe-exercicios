f08e1f73d1f3288fd69d8171fa4236555ede2b32
felipesegala <felipe_segala@outlook.com>
2023-05-05 13:21:50 -0300

    objetos concluidos

--- fundamentos/secao-03-javascript/dia 04 JavaScript Objetos/objetos.js
+++ fundamentos/secao-03-javascript/dia 04 JavaScript Objetos/objetos.js
@@ -31,19 +31,33 @@
 //   console.log(conta['banco']['id']); // 4
 
 
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    agr: 34,
-    medals: { golden: 2, silver: 3 },
-}
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     agr: 34,
+//     medals: { golden: 2, silver: 3 },
+// }
 
-player['besInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
+// player['besInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
 
-console.log('A jogadora', player.name, player.lastName, 'tem', player.agr, 'anos de idade');
+// console.log('A jogadora', player.name, player.lastName, 'tem', player.agr, 'anos de idade');
 
-console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes', player['besInTheWorld']);
+// console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes', player['besInTheWorld']);
 
-console.log('A jogadora possui 2 medalhas de ouro e 3 medalhas de prata', player.medals);
+// console.log('A jogadora possui 2 medalhas de ouro e 3 medalhas de prata', player.medals);
 
-console.table(player);
+// console.table(player);
+
+let band = {
+    name: 'Blind Guardian',
+    formedIn: 1986,
+    lastAlbum: 'Beyond the Red Mirror',
+  };
+  
+  let info = {
+    genre: 'Power Metal',
+    lastAlbum: 'Twilight Orchestra: Legacy of the Dark Lands',
+  };
+
+  Object.assign(band, info);
+  console.log(band);
