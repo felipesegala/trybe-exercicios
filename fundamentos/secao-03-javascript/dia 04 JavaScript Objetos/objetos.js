1c5f1f6205abe2f4bb11539a768f32920a9805f5
felipesegala <felipe_segala@outlook.com>
2023-05-05 10:32:57 -0300

    ex de objetos

--- fundamentos/secao-03-javascript/dia 04 JavaScript Objetos/objetos.js
+++ fundamentos/secao-03-javascript/dia 04 JavaScript Objetos/objetos.js
@@ -11,21 +11,39 @@
 //   diasDaSemana.1; // SyntaxError: Unexpected number
 //   console.log(diasDaSemana['1']); // domingo
 
-let conta = {
-    agencia: '0000',
-    banco: {
-      cod: '012',
-      id: 4,
-      nome: 'TrybeBank',
-    },
-  };
+// let conta = {
+//     agencia: '0000',
+//     banco: {
+//       cod: '012',
+//       id: 4,
+//       nome: 'TrybeBank',
+//     },
+//   };
   
-  let infoDoBanco = 'banco';
-  console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
-  console.log(conta[infoDoBanco]['nome']); // TrybeBank
+//   let infoDoBanco = 'banco';
+//   console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+//   console.log(conta[infoDoBanco]['nome']); // TrybeBank
   
-  console.log(conta.agencia); // 0000
-  console.log(conta['agencia']); // 0000
+//   console.log(conta.agencia); // 0000
+//   console.log(conta['agencia']); // 0000
   
-  console.log(conta.banco.cod); // 012
-  console.log(conta['banco']['id']); // 4
+//   console.log(conta.banco.cod); // 012
+//   console.log(conta['banco']['id']); // 4
+
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    agr: 34,
+    medals: { golden: 2, silver: 3 },
+}
+
+player['besInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
+
+console.log('A jogadora', player.name, player.lastName, 'tem', player.agr, 'anos de idade');
+
+console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes', player['besInTheWorld']);
+
+console.log('A jogadora possui 2 medalhas de ouro e 3 medalhas de prata', player.medals);
+
+console.table(player);
