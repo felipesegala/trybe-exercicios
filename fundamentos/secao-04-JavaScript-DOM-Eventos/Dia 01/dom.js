92d0f68643608da4aa320c04d4a2ee7dd4875ff7
felipesegala <felipe_segala@outlook.com>
2023-05-11 10:02:30 -0300

    nd feito

--- fundamentos/secao-04-JavaScript-DOM-Eventos/Dia 01/dom.js
+++ fundamentos/secao-04-JavaScript-DOM-Eventos/Dia 01/dom.js
@@ -3,3 +3,6 @@ document.getElementById('page-title').innerHTML = 'Kimetsu no Yaiba';
 document.getElementById('paragraph').innerHTML = 'Tanjiro e Nezuko';
 
 document.getElementById('subtitle').innerHTML = 'Inosuke / Zennitsu'
+
+const paragraph = document.getElementsByClassName('paragraph')[0];
+paragraph.style.color = "blue";
