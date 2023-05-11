7fa58f06a95cc3d42d6e4926f88aa10056e1acf9
felipesegala <felipe_segala@outlook.com>
2023-05-11 09:37:22 -0300

    removido arquidos inuteis e add seção 4

---
+++ fundamentos/secao-04-JavaScript-DOM-Eventos/Dia 01/dom.js
@@ -0,0 +1,5 @@
+document.getElementById('page-title').innerHTML = 'Kimetsu no Yaiba';
+
+document.getElementById('paragraph').innerHTML = 'Tanjiro e Nezuko';
+
+document.getElementById('subtitle').innerHTML = 'Inosuke / Zennitsu'
