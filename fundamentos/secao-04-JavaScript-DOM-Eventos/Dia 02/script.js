741bc3ddd286041b2254342d3a637964a0623558
felipesegala <felipe_segala@outlook.com>
2023-05-15 10:29:07 -0300

    ok

---
+++ fundamentos/secao-04-JavaScript-DOM-Eventos/Dia 02/script.js
@@ -0,0 +1,9 @@
+console.log(document.getElementById('parent-of-parent').childNodes);
+document.getElementById('where-are-you').parentNode.style.color = 'red';
+document.getElementById('first-child-of-child').innerText = 'Altera';
+console.log(document.getElementById('parent').firstElementChild);
+console.log(document.getElementById('where-are-you').previousElementSibling);
+console.log(document.getElementById('where-are-you').parentNode);
+console.log(document.getElementById('where-are-you').nextElementSibling);
+console.log(document.getElementById('parent').nextElementSibling);
+
